a2643b2f35730f17eea0cf7b2cb1479d339590d9
Leonardo Aguiar <leolhulaguiar@gmail.com>
2026-05-09 09:41:02 -0300

    chore: inicializa solucao e projeto

---
+++ TesteEndereco.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TesteEndereco.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
