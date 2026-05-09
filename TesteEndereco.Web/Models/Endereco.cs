878d5592e9b6a568680726a0bad1920b1377c177
Leonardo Aguiar <leolhulaguiar@gmail.com>
2026-05-09 20:37:08 -0300

    ajustado nomenclaturas requerimento Endereco.cs

--- TesteEndereco.Web/Models/Endereco.cs
+++ TesteEndereco.Web/Models/Endereco.cs
@@ -7,30 +7,30 @@ namespace TesteEndereco.Web.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Informe o CEP.")]
         [StringLength(8)]
         public string Cep { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe o Logradouro.")]
         [StringLength(200)]
         public string Logradouro { get; set; } = string.Empty;
 
         [StringLength(200)]
         public string? Complemento { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Informe o Bairro.")]
         [StringLength(100)]
         public string Bairro { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe a Cidade.")]
         [StringLength(100)]
         public string Cidade { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe a UF.")]
         [StringLength(2)]
         public string Uf { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = "Informe o Numero.")]
         [StringLength(20)]
         public string Numero { get; set; } = string.Empty;
 
